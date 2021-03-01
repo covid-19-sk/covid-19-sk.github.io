6e9f43e86afdab1617f2e7b623fbeff8467a28f7
covid-19-sk <75218751+covid-19-sk@users.noreply.github.com>
2021-03-01 10:17:49 +0100

    fix main.js

--- js/main.js
+++ js/main.js
@@ -26,16 +26,16 @@ $(document).scroll(function() {
 $.getJSON('https://api.apify.com/v2/key-value-stores/GlTLAdXAuOz6bLAIO/records/LATEST?disableRedirect=true', function(data) {
   let infected_locale = new Number(`${data.infectedPCR}`).toLocaleString("sk-SK");
   let deaths_locale = new Number(`${data.deceased}`).toLocaleString("sk-SK");
-  let recovered_locale = new Number(`${data.recovered}`).toLocaleString("sk-SK");
+  let recovered_locale = new Number(`${data.infectedAG}`).toLocaleString("sk-SK");
   let newInfectedPCR_locale = new Number(`${data.newInfectedPCR}`).toLocaleString("sk-SK");
   let newDeceased_locale = new Number(`${data.newDeceased}`).toLocaleString("sk-SK");
-  let newRecovered_locale = new Number(`${data.newRecovered}`).toLocaleString("sk-SK");
+  let newRecovered_locale = new Number(`${data.newInfectedAG}`).toLocaleString("sk-SK");
   let testedPCR = new Number(`${data.testedPCR}`).toLocaleString("sk-SK");
   let newPCR_locale = new Number(`${data.newInfectedPCR}`).toLocaleString("sk-SK");
   let newInfectedTodayTotal = new Number(`${data.newInfectedPCR}`) + new Number(`${data.newInfectedAG}`);
   let newInfectedTodayTotal_locale = newInfectedTodayTotal.toLocaleString("sk-SK");
   let newTestedPCR = new Number(`${data.newTestedPCR}`).toLocaleString("sk-SK");
-  let date = (`${data.updated}`);
+  let date = (`${data.lastUpdatedAtSource}`);
   let year = date.slice(date.length - 4);
   let newdate = date.substring(0, date.length - 4);
 
@@ -54,7 +54,7 @@ $.getJSON('https://api.apify.com/v2/key-value-stores/GlTLAdXAuOz6bLAIO/records/L
   $("#testedPCR").html(testedPCR);
   $("#newInfectedPCR").html('<i class="fas fa-arrow-up red-color"></i> ' + newInfectedPCR_locale);
   $("#newDeceased").html('<i class="fas fa-arrow-up red-color"></i> ' + newDeceased_locale);
-  $("#newRecovered").html('<i class="fas fa-arrow-up green-color2"></i> ' + newRecovered_locale);
+  $("#newRecovered").html('<i class="fas fa-arrow-up red-color"></i> ' + newRecovered_locale);
   $("#newTestedPCR").html('<i class="fas fa-arrow-up green-color2"></i> ' + newTestedPCR);
   $("#date").html(newdate);
   $("#year").html(year);
